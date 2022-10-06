65433cb2417b1427c36da2500e4f7cc73352c6dc
Vincent-Johannes Schnitzbauer <vincent.schnitzbauer@rohde-schwarz.com>
2022-10-06 11:44:33 +0200

    provide current matched word in callback as parameter

--- keyboard/__init__.py
+++ keyboard/__init__.py
@@ -1224,7 +1224,7 @@ def add_word_listener(word, callback, triggers=['space'], match_suffix=False, ti
 
         matched = state.current == word or (match_suffix and state.current.endswith(word))
         if name in triggers and matched:
-            callback()
+            callback(state.current)
             state.current = ''
         elif len(name) > 1:
             state.current = ''
